61769c835c0b737bfd9491d16e06565d4d9090b1
donghyun0502 <100737090+donghyun0502@users.noreply.github.com>
2024-01-28 02:58:37 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 180 ms, Memory: 15684 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -2,35 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		StringBuilder sb = new StringBuilder();
 		
-		int N = Integer.parseInt(st.nextToken());//행
-		int M = Integer.parseInt(st.nextToken());//열
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		
 		int[][] A = new int[N][M];
 		int[][] B = new int[N][M];
 		
 		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine()," ");
 			for(int j=0;j<M;j++) {
 				A[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
+		
 		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine()," ");
 			for(int j=0;j<M;j++) {
 				B[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
+	
 		for(int i=0;i<N;i++) {
 			for(int j=0;j<M;j++) {
-				sb.append(A[i][j] + B[i][j] + " ");
+				sb.append(A[i][j]+B[i][j]).append(" ");
 			}
 			sb.append("\n");
 		}
