8a2861509cc9489bc168ca4d9f87bf344024e581
donghyun0502 <100737090+donghyun0502@users.noreply.github.com>
2024-01-28 01:28:15 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -2,29 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+
+	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
-		int N = Integer.parseInt(br.readLine()); // 정수의 개수
-		int[] A = new int[N]; 
+		int N = Integer.parseInt(br.readLine());
 		
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		int arr[] = new int[N];
 		
-		for(int i=0;i<A.length;i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		int cnt=0;
 		int v = Integer.parseInt(br.readLine());
-		int cnt = 0;
 		
-		for(int i=0;i<A.length;i++) {
-			if(A[i]==v) {
+		for(int i=0;i<N;i++) {
+			if(arr[i]==v)
 				cnt++;
-			}
 		}
 		System.out.println(cnt);
-		br.close();
 	}
 }
