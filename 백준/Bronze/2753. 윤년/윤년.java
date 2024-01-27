b58fa3b70ad749f52dbf66c27926dfc05ee5209c
donghyun0502 <100737090+donghyun0502@users.noreply.github.com>
2024-01-27 18:17:17 +0900

    [Bronze V] Title: 윤년, Time: 132 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -3,17 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	
-	static int year(int y) {
-		return(y%4==0 && y%100!=0 || y%400==0 ? 1:0);
-	}
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int y = Integer.parseInt(br.readLine());
+		int X = Integer.parseInt(br.readLine());
 		
-		System.out.println(year(y));
+		if(X%4==0 && X%100!=0 || X%400==0) {
+			System.out.println("1");
+		}else
+		{
+			System.out.println("0");
+		}
+		
 	}
 }
