99483fd5185d05c58b611e2e6524f52df9061745
donghyun0502 <100737090+donghyun0502@users.noreply.github.com>
2024-01-30 04:29:05 +0900

    [Bronze V] Title: 문자열, Time: 116 ms, Memory: 13964 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,21 +1,20 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int testcase = Integer.parseInt(br.readLine());
-		String arr[] = new String[testcase];
-		
-		for(int i=0;i<testcase;i++) {
-			arr[i] = br.readLine();
-		}
-		
 		StringBuilder sb = new StringBuilder();
-		for(int i=0;i<testcase;i++) {
-			sb.append(arr[i].charAt(0)).append(arr[i].charAt(arr[i].length()-1)).append("\n");
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<num;i++) {
+			String chr = br.readLine();
+			char[] arr = chr.toCharArray();
+			
+			sb.append(arr[0]).append(arr[arr.length-1]).append("\n");
 		}
 		System.out.println(sb);
 	}
